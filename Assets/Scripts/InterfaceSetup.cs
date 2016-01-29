c06cb350e9fd6f91957344b520227d48c3ca0d1a
StealthTom <seafordtbf@gmail.com>
2016-01-29 16:26:11 -0500

    Added the project.

---
+++ Assets/Scripts/InterfaceSetup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceSetup : MonoBehaviour {
+
+    public Transform[] twoPlayerPoints;
+    public Transform[] threePlayerPoints;
+    public Transform[] fourPlayerPoints;
+
+    public GameObject playerInterface;
+
+    public bool SetupUI(int playerNum)
+    {
+        switch (playerNum)
+        {
+            case 2:
+                for (int count = 0; count < playerNum; count++)
+                {
+                    Transform _temp = Instantiate(playerInterface).GetComponent<Transform>();
+                    _temp.position = twoPlayerPoints[count].position;
+                }
+                return true;
+
+            case 3:
+                for (int count = 0; count < playerNum; count++)
+                {
+                    Transform _temp = Instantiate(playerInterface).GetComponent<Transform>();
+                    _temp.position = threePlayerPoints[count].position;
+                }
+                return true;
+
+            case 4:
+                for (int count = 0; count < playerNum; count++)
+                {
+                    Transform _temp = Instantiate(playerInterface).GetComponent<Transform>();
+                    _temp.position = fourPlayerPoints[count].position;
+                }
+                return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is a utility script.
+/// Anyone who needs to be a singlton should derive from this object
+/// When they do, they will automatically be a singleton.
+/// Used for convenience.
+/// </summary>
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance;
+
+    public static T instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T)); //makes the concrete implemenation a singleton
+            }
+            return _instance;
+        }
+    }
+}
